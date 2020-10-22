e82f3136bd66eff5f9586b012c896bec778234d5
Saturn Karry <skarry@si-d1.kzn.21-school.ru>
2020-10-22 12:55:53 +0300

    empty

--- mini/ft_start.c
+++ mini/ft_start.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_start.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: skarry <skarry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 17:13:10 by atomatoe          #+#    #+#             */
-/*   Updated: 2020/10/22 12:25:27 by atomatoe         ###   ########.fr       */
+/*   Updated: 2020/10/22 12:47:06 by skarry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 int ft_start (t_commands *cmd, t_data *all)
 {
+    if (cmd->cmd)
+    {
     if(ft_compare_str(cmd->cmd, "pwd") == 1)
         ft_give_pwd(cmd);
     else if(ft_compare_str(cmd->cmd, "env") == 1)
@@ -29,5 +31,6 @@ int ft_start (t_commands *cmd, t_data *all)
     // else if(ft_compare_str(cmd->cmd, "ls -la") == 1)
     //     ft_give_command(cmd, all);  // добавить в поиск директории исполняемого файла флаг all->flag_command; если директория найдена флаг = 1;
     //                                 // условием запуска этой команды будет наличие флаг = 1;
+    }
     return(0);
 }
