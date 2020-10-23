7fee7f1ac0070240871d7617743645d856dd31f5
Saturn Karry <skarry@si-d1.kzn.21-school.ru>
2020-10-23 17:22:12 +0300

    add ;

--- mini/ft_start.c
+++ mini/ft_start.c
@@ -3,33 +3,56 @@
 /*                                                        :::      ::::::::   */
 /*   ft_start.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: atomatoe <atomatoe@student.42.fr>          +#+  +:+       +#+        */
+/*   By: skarry <skarry@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/21 17:13:10 by atomatoe          #+#    #+#             */
-/*   Updated: 2020/10/22 17:59:02 by atomatoe         ###   ########.fr       */
+/*   Updated: 2020/10/23 17:19:54 by skarry           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
+void	do_cmd(t_commands *cmd, t_data *all)
+{
+	if (cmd->cmd)
+	{
+		if(ft_compare_str(cmd->cmd, "pwd") == 1)
+			ft_give_pwd(cmd);
+		else if(ft_compare_str(cmd->cmd, "env") == 1)
+			ft_give_env(cmd, all);
+		else if(ft_compare_str(cmd->cmd, "echo") == 1)
+			ft_give_echo(cmd);
+		else if(ft_compare_str(cmd->cmd, "export") == 1)
+			ft_give_export(cmd, all);
+		else if(ft_compare_str(cmd->cmd, "unset") == 1)
+			ft_give_unset(cmd, all);
+		else if(ft_compare_str(cmd->cmd, "cd") == 1)
+			ft_give_cd(cmd, all);
+		else if(cmd->dir_find == 1)
+			ft_give_command(cmd, all);
+	}
+
+}
 
 int ft_start (t_commands *cmd, t_data *all)
 {
-    if (cmd->cmd)
-    {
-        if(ft_compare_str(cmd->cmd, "pwd") == 1)
-            ft_give_pwd(cmd);
-        else if(ft_compare_str(cmd->cmd, "env") == 1)
-            ft_give_env(cmd, all);
-        else if(ft_compare_str(cmd->cmd, "echo") == 1)
-            ft_give_echo(cmd);
-        else if(ft_compare_str(cmd->cmd, "export") == 1)
-            ft_give_export(cmd, all);
-        else if(ft_compare_str(cmd->cmd, "unset") == 1)
-            ft_give_unset(cmd, all);
-        else if(ft_compare_str(cmd->cmd, "cd") == 1)
-            ft_give_cd(cmd, all);
-        else if(cmd->dir_find == 1)
-            ft_give_command(cmd, all);
-    }
-    return(0);
+	// t_commands	*pipe;
+	t_commands	*next;
+	// t_commands	*redir;
+
+	next = cmd;
+	while (next)
+	{
+		// pipe = next;
+		// while (pipe)
+		// {
+		// 	give_directory(pipe, all);
+		// 	redir = pipe->redir;
+		// 	if (redir)
+		// 		record_arg(pipe, redir);
+		// 	pipe = pipe->pipe;
+		// }
+		do_cmd(next, all);
+		next = next->next;
+	}
+	return(0);
 }
